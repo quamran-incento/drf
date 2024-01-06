dc018a4d5776c9609be525b8a9f3b1b747f1408b
Quamran Hasan <quamran@Quamrans-MacBook-Pro.local>
2024-01-07 02:16:44 +0900

    add

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
+endpoint = "http://localhost:8001/api/" #http://127.0.0.1:8000/ 
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
 print(get_response.text) # print raw text response
